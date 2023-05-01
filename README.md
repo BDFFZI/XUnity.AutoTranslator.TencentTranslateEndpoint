bd1630675f74b2ca90a76a2ecb7c08b3071a5239
BDFFZI <1330958515@qq.com>
2023-05-01 11:42:35 +0800

    优化了翻译方式，现在单词会被当成句子翻译，以保证适合上下文

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 - 更快的并发速度（每秒查询次数为5，百度只有1）
 - 更多的翻译次数（每月支持5百万字符，百度只有5万）
 ## 问题
-百度翻译也有该问题，翻译调用速度过快时，偶尔会出现无法翻译的情况，建议配合XUnity.AutoTranslator的失败回调功能处理。
+- 百度翻译也有该问题，翻译调用速度过快时，偶尔会出现无法翻译的情况，建议配合XUnity.AutoTranslator的失败回调功能处理。
+- 在UnityInjector框架下无法识别到该后端，但BepInEx可以，尝试了混合使用，好在没有问题。
 ## 使用
 放在XUnity.AutoTranslator插件目录的Translators文件夹下，翻译插件都是统一放在那的，相关信息在XUnity.AutoTranslator中都有说明。
--- TencentTranslate/TencentSignatureV1.cs
+++ TencentTranslate/TencentSignatureV1.cs
@@ -9,59 +9,62 @@ using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Text;
 
-/// 用于实现腾讯v1版本签名的函数
-/// https://cloud.tencent.com/document/api/551/15616
-public static class TencentSignatureV1
+namespace TencentTranslate
 {
-    /// <summary>
-    /// 生成针对请求信息的数字签名
-    /// </summary>
-    /// <param name="requestMethod">HTTP的请求方法：本项目基于“POST”方法</param>
-    /// <param name="requestHost">HTTP的通信服务器：文本翻译的服务器为“tmt.tencentcloudapi.com”</param>
-    /// <param name="requestPath">API请求路径：文本翻译的API请求路径为“/”</param>
-    /// <param name="requestParams">API请求参数</param>
-    /// <param name="signKey">用于签名的密钥</param>
-    /// <returns></returns>
-    public static string MakeRequestSignature(string requestMethod, string requestHost, string requestPath, Dictionary<string, string> requestParams, string signKey)
+    /// 用于实现腾讯v1版本签名的函数
+    /// https://cloud.tencent.com/document/api/551/15616
+    public static class TencentSignatureV1
     {
-        //拼接用于签名的字符串原文
-        StringBuilder message = new StringBuilder();
-        message.Append(requestMethod);
-        message.Append(requestHost);
-        message.Append(requestPath);
-        message.Append("?");
-        foreach (KeyValuePair<string, string> param in requestParams)
+        /// <summary>
+        /// 生成针对请求信息的数字签名
+        /// </summary>
+        /// <param name="requestMethod">HTTP的请求方法：本项目基于“POST”方法</param>
+        /// <param name="requestHost">HTTP的通信服务器：文本翻译的服务器为“tmt.tencentcloudapi.com”</param>
+        /// <param name="requestPath">API请求路径：文本翻译的API请求路径为“/”</param>
+        /// <param name="requestParams">API请求参数</param>
+        /// <param name="signKey">用于签名的密钥</param>
+        /// <returns></returns>
+        public static string MakeRequestSignature(string requestMethod, string requestHost, string requestPath, Dictionary<string, string> requestParams, string signKey)
         {
-            message.Append(param.Key);
-            message.Append("=");
-            message.Append(param.Value);
-            message.Append("&");
-        }
-        message.Length--;
+            //拼接用于签名的字符串原文
+            StringBuilder message = new StringBuilder();
+            message.Append(requestMethod);
+            message.Append(requestHost);
+            message.Append(requestPath);
+            message.Append("?");
+            foreach (KeyValuePair<string, string> param in requestParams)
+            {
+                message.Append(param.Key);
+                message.Append("=");
+                message.Append(param.Value);
+                message.Append("&");
+            }
+            message.Length--;
 
-        //使用HMAC-SHA1算法对原文进行签名
-        using (HMACSHA1 sha1 = new HMACSHA1(Encoding.UTF8.GetBytes(signKey)))
-        {
-            byte[] signature = sha1.ComputeHash(Encoding.UTF8.GetBytes(message.ToString()));
-            return Convert.ToBase64String(signature);
+            //使用HMAC-SHA1算法对原文进行签名
+            using (HMACSHA1 sha1 = new HMACSHA1(Encoding.UTF8.GetBytes(signKey)))
+            {
+                byte[] signature = sha1.ComputeHash(Encoding.UTF8.GetBytes(message.ToString()));
+                return Convert.ToBase64String(signature);
+            }
         }
-    }
-    /// <summary>
-    /// 生成用于Post的数据
-    /// </summary>
-    /// <param name="requestParams"></param>
-    /// <returns></returns>
-    public static string MakeParameterUriString(Dictionary<string, string> requestParams)
-    {
-        StringBuilder result = new StringBuilder();
-        foreach (KeyValuePair<string, string> param in requestParams)
+        /// <summary>
+        /// 生成用于Post的数据
+        /// </summary>
+        /// <param name="requestParams"></param>
+        /// <returns></returns>
+        public static string MakeParameterUriString(Dictionary<string, string> requestParams)
         {
-            result.Append(param.Key);
-            result.Append("=");
-            result.Append(Uri.EscapeDataString(param.Value));
-            result.Append("&");
+            StringBuilder result = new StringBuilder();
+            foreach (KeyValuePair<string, string> param in requestParams)
+            {
+                result.Append(param.Key);
+                result.Append("=");
+                result.Append(Uri.EscapeDataString(param.Value));
+                result.Append("&");
+            }
+            result.Length--;
+            return result.ToString();
         }
-        result.Length--;
-        return result.ToString();
     }
 }
